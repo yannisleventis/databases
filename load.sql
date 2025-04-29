eb5542b434b1f5237f30987601bcaef2a77637ef
Vasilis Afentoulidis <billafent@gmail.com>
2025-04-29 14:55:40 +0300

    New

---
+++ load.sql
@@ -0,0 +1,28 @@
+
+
+
+-- Lookup Tables
+
+-- Experience level
+INSERT INTO Experience_Level (Level_Name) VALUES
+('novice'), ('beginner'), ('intermediate'), ('experienced'), ('expert');
+
+-- Payment method
+INSERT INTO Payment_Method (Method_Name) VALUES
+('credit_card'), ('debit_card'), ('bank_account'), ('cash');
+
+-- Ticket Category
+INSERT INTO Ticket_Category (Category_Name) VALUES
+('General'), ('VIP'), ('Backstage');
+
+-- Ticket Status
+INSERT INTO Ticket_Status (Status_Name) VALUES
+('active'), ('used');
+
+-- Resale Status
+INSERT INTO Resale_Status (Status_Name) VALUES
+('available'), ('sold');
+
+-- Continent
+INSERT INTO Continent (Continent_Name) VALUES
+('Europe'), ('Asia'), ('Africa'), ('North America'), ('South America'), ('Australia'), ('Antarctica');
